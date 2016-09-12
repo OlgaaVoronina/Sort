3f5da46f36940b1fd0eae6deac5292acc5b891a0
OlgaaVoronina <wtsought@gmail.com>
2016-09-12 15:25:40 +0300

    Update SortTest.java

--- src/test/java/ua/epam/SortTest.java
+++ src/test/java/ua/epam/SortTest.java
@@ -11,7 +11,13 @@ public class SortTest {
 
     private Sort sort;
 
-    int[] arr = {1, 5, 3, 2, 4};
+    int[] arr00 = {1, 5, 3, 2, 4};
+    int[] arr01 = {1, 5, 3, 2, 4};
+    int[] arr02 = {1, 5, 3, 2, 4};
+    int[] arr03 = {1, 5, 3, 2, 4};
+    int[] arr04 = {1, 5, 3, 2, 4};
+    int[] arr05 = {1, 5, 3, 2, 4};
+    
     int[] arr1 = {1, 2, 3, 4, 5};
 
     @Before
@@ -20,51 +26,51 @@ public class SortTest {
     //buble
     @Test
     public void bubbleSort(){
-        sort.bubbleSort(arr);
-        Assert.assertArrayEquals(arr, arr1);
+        sort.bubbleSort(arr00);
+        Assert.assertArrayEquals(arr00, arr1);
     }
 
     @Test
     public void bubbleSort1(){
-        int size = (sort.bubbleSort(arr)).length;
+        int size = (sort.bubbleSort(arr00)).length;
         Assert.assertTrue( size == 5);
     }
 
     //selection
     @Test
     public void selectionSort() {
-        sort.selectionSort(arr);
-        Assert.assertArrayEquals(arr, arr1);
+        sort.selectionSort(arr01);
+        Assert.assertArrayEquals(arr01, arr1);
     }
 
     @Test
     public void selectionSort1(){
-        int size = (sort.selectionSort(arr)).length;
+        int size = (sort.selectionSort(arr01)).length;
         Assert.assertTrue( size == 5);
     }
 
     //insertion
     @Test
     public void insertionSort(){
-        sort.insertionSort(arr);
-        Assert.assertArrayEquals(arr, arr1);
+        sort.insertionSort(arr02);
+        Assert.assertArrayEquals(arr02, arr1);
     }
 
     @Test
     public void insertionSort1(){
-        int size = (sort.insertionSort(arr)).length;
+        int size = (sort.insertionSort(arr02)).length;
         Assert.assertFalse( size == 10);
     }
 
     //quick
     @Test
     public void quickSort(){
-        sort.quickSort(arr, 0, 4);
-        Assert.assertArrayEquals(arr, arr1);
+        sort.quickSort(arr03, 0, 4);
+        Assert.assertArrayEquals(arr03, arr1);
     }
 
     public void quickSort1(){
-        int size = (sort.quickSort(arr, 0, 4)).length;
+        int size = (sort.quickSort(arr03, 0, 4)).length;
         int sizeArr1 = arr1.length;
         Assert.assertTrue( size == sizeArr1);
     }
@@ -72,15 +78,15 @@ public class SortTest {
     //shella
     @Test
     public void shellaSort() {
-        sort.shellaSort(arr);
-        Assert.assertArrayEquals(arr, arr1);
+        sort.shellaSort(arr04);
+        Assert.assertArrayEquals(arr04, arr1);
     }
 
     //gnome
     @Test
     public void gnomeSort() {
-        sort.gnomeSort(arr);
-        Assert.assertArrayEquals(arr, arr1);
+        sort.gnomeSort(arr05);
+        Assert.assertArrayEquals(arr05, arr1);
     }
 
 }
