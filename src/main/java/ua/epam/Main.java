9f360126a7810fdb710a2d6a6ed6244ffbbcaa2e
OlgaaVoronina <wtsought@gmail.com>
2016-09-12 15:17:41 +0300

    Update Main.java

--- src/main/java/ua/epam/Main.java
+++ src/main/java/ua/epam/Main.java
@@ -7,6 +7,12 @@ public class Main {
     public static void main(String[] args) {
 
         int[] arr = {8, 7, 4, 5, 7, 81, 2, 5, 5, 1, 0, 45, 1, 85};
+        int[] arr1 = {8, 7, 4, 5, 7, 81, 2, 5, 5, 1, 0, 45, 1, 85};
+        int[] arr2 = {8, 7, 4, 5, 7, 81, 2, 5, 5, 1, 0, 45, 1, 85};
+        int[] arr3 = {8, 7, 4, 5, 7, 81, 2, 5, 5, 1, 0, 45, 1, 85};
+        int[] arr4 = {8, 7, 4, 5, 7, 81, 2, 5, 5, 1, 0, 45, 1, 85};
+        int[] arr5 = {8, 7, 4, 5, 7, 81, 2, 5, 5, 1, 0, 45, 1, 85};
+
         System.out.println("Input array = " + Arrays.toString(arr));
 
         int left = 0;          //first index of elements
@@ -17,19 +23,19 @@ public class Main {
         sort.bubbleSort(arr);
         System.out.println("\nBubble sort array = " + Arrays.toString(arr));
 
-        sort.selectionSort(arr);
+        sort.selectionSort(arr1);
         System.out.println("Selection sort array = " + Arrays.toString(arr));
 
-        sort.insertionSort(arr);
+        sort.insertionSort(arr2);
         System.out.println("Insertion sort array = " + Arrays.toString(arr));
 
-        sort.quickSort(arr, left, right);
+        sort.quickSort(arr3, left, right);
         System.out.println("Quick sort array = " + Arrays.toString(arr));
 
-        sort.shellaSort(arr);
+        sort.shellaSort(arr4);
         System.out.println("Shella sort array = " + Arrays.toString(arr));
 
-        sort.gnomeSort(arr);
+        sort.gnomeSort(arr5);
         System.out.println("Gnome sort array = " + Arrays.toString(arr));
 
 
